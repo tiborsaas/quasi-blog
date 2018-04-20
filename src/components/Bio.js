4c9eefde95778fba9e4fa210e8219cdf73754b14
Szász Tibor <tszasz@gmail.com>
2018-04-20 02:48:24 +0200

    initial version

---
+++ src/components/Bio.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <p>
+          Under construction, powered by Gatsby.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
---
+++ src/pages/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+
+import Bio from '../components/Bio'
+import { rhythm } from '../utils/typography'
+
+class BlogIndex extends React.Component {
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const posts = get(this, 'props.data.allMarkdownRemark.edges')
+
+    return (
+      <div>
+        <Helmet title={siteTitle} />
+        <h1>Quasi ink</h1>
+        <Bio />
+      </div>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "DD MMMM, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
